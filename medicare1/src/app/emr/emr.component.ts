6e36fc5901efac8d9462fa06f6daf44f28442c7b
KKShaminda <shamindakaveesha72@gmail.com>
2025-04-26 19:52:40 +0530

    Recreate environment with bootstrap

---
+++ medicare1/src/app/emr/emr.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emr',
+  imports: [],
+  templateUrl: './emr.component.html',
+  styleUrl: './emr.component.css'
+})
+export class EmrComponent {
+
+}
