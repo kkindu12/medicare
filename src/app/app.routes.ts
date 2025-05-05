cea85f05104d164a787a7226a2a0bcb05abc7a7c
kkindu12 <164643470+kkindu12@users.noreply.github.com>
2025-05-06 01:57:55 +0530

    Step 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BookingComponent } from './booking/booking.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'booking', component: BookingComponent },
+  { path: '**', redirectTo: '' }
+];
