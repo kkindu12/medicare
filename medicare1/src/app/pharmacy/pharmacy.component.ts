f7d5428976bf30f206937158ec1d92e4f8ca0052
SPBT2002 <164739735+SPBT2002@users.noreply.github.com>
2025-05-09 14:51:23 +0530

    Setup files

---
+++ medicare1/src/app/pharmacy/pharmacy.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pharmacy',
+  imports: [],
+  templateUrl: './pharmacy.component.html',
+  styleUrl: './pharmacy.component.css'
+})
+export class PharmacyComponent {
+
+}
