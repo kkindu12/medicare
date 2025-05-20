c36a87b99db065f2621231335bd49ac0482cd3dd
kkindu12 <164643470+kkindu12@users.noreply.github.com>
2025-05-20 17:48:06 +0530

    working model

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { BookingComponent } from './booking/booking.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'booking', component: BookingComponent },
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: '/login' }
 ];
