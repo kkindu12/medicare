74cecc816e67947ab2fc169cbfcaddebfc0d41ef
kayaththiri-kanagasabai <kaya282002@gmail.com>
2025-05-24 18:58:43 +0530

    commit changes

--- medicare1/src/app/app.component.ts
+++ medicare1/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrescriptionFormComponent } from './prescription-form/prescription-form.component';
+import { ChatPageComponent } from './chat-page/chat-page.component';
+import { AIDrivenPageComponent } from './ai-driven-page/ai-driven-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,PrescriptionFormComponent,ChatPageComponent,AIDrivenPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
