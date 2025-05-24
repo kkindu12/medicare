fb40a8359b504958e47f07c67a8dc9676e94a9d4
kayaththiri-kanagasabai <kaya282002@gmail.com>
2025-05-24 18:47:32 +0530

    commit changes

---
+++ medicare1/src/app/ai-driven-page/ai-driven-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ai-driven-page',
+  imports: [],
+  templateUrl: './ai-driven-page.component.html',
+  styleUrl: './ai-driven-page.component.css'
+})
+export class AIDrivenPageComponent {
+
+}
