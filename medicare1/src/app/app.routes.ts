b2c127009077bf70a78eedfefa9bb9ddb8216655
KKShaminda <shamindakaveesha72@gmail.com>
2025-05-28 22:55:02 +0530

    make routing

--- medicare1/src/app/app.routes.ts
+++ medicare1/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { SigninComponent } from './pages/signin/signin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'signin', component: SigninComponent },
+
+];
