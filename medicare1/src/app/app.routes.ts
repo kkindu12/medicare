5d73bbf062dc19f4d4db74be3e8b5dfe19465a67
KKShaminda <shamindakaveesha72@gmail.com>
2025-06-01 22:42:14 +0530

    added authguard to secure apis

--- medicare1/src/app/app.routes.ts
+++ medicare1/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { SigninComponent } from './pages/signin/signin.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { EmrComponent } from './emr/emr.component';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/signin', pathMatch: 'full' },
     { path: 'signin', component: SigninComponent },
     { path: 'signup', component: SignupComponent },
-    { path: 'emr', component: EmrComponent },
+    { path: 'emr', component: EmrComponent, canActivate: [AuthGuard] },
 ];
