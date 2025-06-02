6791b42ffa532d4aaf25e035b5cfac81bd158527
Rashini1 <164763715+Rashini1@users.noreply.github.com>
2025-06-02 12:06:22 +0530

    payment successful componentts file

---
+++ payment-success.component.ts
@@ -0,0 +1,13 @@
+ import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-payment-success',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './payment-success.component.html',
+  styleUrls: ['./payment-success.component.scss']
+})
+export class PaymentSuccessComponent {}
+
