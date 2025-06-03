b91b448f32c90965a5712a3d75d00e27b5350f3c
KKShaminda <shamindakaveesha72@gmail.com>
2025-06-03 15:41:17 +0530

    implemented medicine and labtest get apis

---
+++ medicare2/models/medicine.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from typing import List, Optional
+from datetime import datetime
+
+class Medicine(BaseModel):
+    id: Optional[str] = Field(alias="_id")
+    name: str
+    description: str
+    dosage: str
+    sideEffects: str
+    manufacturer: str
+    expiryDate: datetime
+    stock: int
+    price: str
