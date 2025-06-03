c328d66f4d3c6941ab67487d281673830e07eff7
KKShaminda <shamindakaveesha72@gmail.com>
2025-06-03 16:39:55 +0530

    updated signup for doctors

--- medicare1/src/app/emr/models/User.ts
+++ medicare1/src/app/emr/models/User.ts
@@ -1,3 +1,14 @@
+export interface DoctorDetails {
+  usualStartingTime: string;
+  usualEndingTime: string;
+  experience: number;
+  highestQualifications: string;
+  registrationNumber: string;
+  registrationAuthority: string;
+  registrationDate: string;
+  feeForAppointment: number;
+}
+
 export interface User {
   _id?: string; // Optional if you're generating it server-side
   firstName: string;
@@ -6,6 +17,7 @@ export interface User {
   phoneNumber: string; // ISO date string (e.g., "2025-05-25")
   password: string; // e.g., "13:42"
   role: boolean; // Same as above
+  doctorDetails?: DoctorDetails; // Optional, only for doctors
 }
 
 export interface GetUserDTO{
