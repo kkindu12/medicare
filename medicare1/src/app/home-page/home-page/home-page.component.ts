1b149df7a05a0721b70ab7eaa58d38490a2e92ce
kkindu12 <164643470+kkindu12@users.noreply.github.com>
2025-06-07 10:25:14 +0530

    create home page

---
+++ medicare1/src/app/home-page/home-page/home-page.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.scss'
+})
+export class HomePageComponent {
+  constructor(private router: Router) {}
+
+  goToSignIn() {
+    this.router.navigate(['/signin']);
+  }
+}
