8285d44c94ab5fe738b45d7acade73a30ab51b61
SPBT2002 <164739735+SPBT2002@users.noreply.github.com>
2025-06-11 13:30:06 +0530

    setup files

---
+++ medicare1/src/app/pharmacy_dashboard/pharmacy&laboratory.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pharmacy',
+  imports: [],
+  templateUrl: './pharmacy&laboratory.component.html',
+  styleUrl: './pharmacy&laboratory.component.scss'
+})
+export class PharmacyComponent {
+
+}
