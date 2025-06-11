da52acb8d26121f5567db553c64d479661ca9d77
kkindu12 <164643470+kkindu12@users.noreply.github.com>
2025-06-11 12:26:07 +0530

    home page  (#13)
    * create home page
    
    * modify home page

--- medicare1/src/app/app.routes.ts
+++ medicare1/src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { SigninComponent } from './pages/signin/signin.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { EmrComponent } from './emr/emr.component';
 import { AuthGuard } from './guards/auth.guard';
+import { HomePageComponent } from './home-page/home-page/home-page.component';
+import { BookingAppointment } from './bam/booking-appointment/booking-appointment';
+
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/signin', pathMatch: 'full' },
+    { path: '', component: HomePageComponent },
     { path: 'signin', component: SigninComponent },
     { path: 'signup', component: SignupComponent },
     { path: 'emr', component: EmrComponent, canActivate: [AuthGuard] },
-    { path: '**', redirectTo: '/signin' }
+    { path: 'appointment-booking', component: BookingAppointment },
+    { path: '**', redirectTo: '' }
 ];
